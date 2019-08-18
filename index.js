dc4e1d48ebb0c3e2df242698cbd2042af8090fec
fortunate-commit-9523 <alexiscash97@gmail.com>
2019-08-18 20:09:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function whisper(string) {
 function logShout(string) {
   console.log(string);
 }
+
+function logWhisper(string) {
+  console.log(string);
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma") {
+    return "I love you, too."
+  }
+}
